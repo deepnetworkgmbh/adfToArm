a082923be61fbf0e0dd9f1a76e7bcd99c2f3ba07
ihork <igor.kliushnikov@hotmail.com>
2017-10-04 18:38:50 +0300

    ArmParameter model with JsonConverter for it

--- AdfToArm/Models/ARM/ArmParameter.cs
+++ AdfToArm/Models/ARM/ArmParameter.cs
@@ -1,6 +1,35 @@
-﻿namespace AdfToArm.Models.ARM
+﻿using Newtonsoft.Json;
+
+namespace AdfToArm.Models.ARM
 {
+    [JsonConverter(typeof(ArmParameterConverter))]
     public class ArmParameter
     {
+        public string Name { get; set; }
+
+        public ArmParameterProperties Properties { get; set; }
+    }
+
+    [JsonObject]
+    public class ArmParameterProperties
+    {
+        [JsonProperty("type", Required = Required.Always)]
+        public string Type { get; set; }
+
+        [JsonProperty("defaultValue", Required = Required.Default)]
+        public object DefaultValue { get; set; }
+
+        [JsonProperty("allowedValues", Required = Required.Default)]
+        public object[] AllowedValues { get; set; }
+
+        [JsonProperty("metadata", Required = Required.Default)]
+        public ParameterMetadata Metadata { get; set; }
+    }
+
+    [JsonObject]
+    public class ParameterMetadata
+    {
+        [JsonProperty("description", Required = Required.Always)]
+        public string Description { get; set; }
     }
 }
---
+++ AdfToArm/Models/ARM/ArmParameterConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AdfToArm.Models.ARM
+{
+    public class ArmParameterConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(ArmParameter);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var parameter = value as ArmParameter;
+
+            if (parameter == null)
+                return;
+
+            JObject jo = new JObject();
+            var propertiesToken = JToken.FromObject(parameter.Properties);
+            jo.Add(parameter.Name, propertiesToken);
+
+            jo.WriteTo(writer);
+        }
+    }
+}
